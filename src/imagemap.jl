4a01b452201bfe2bfbd16c8b4d0421d698052232
Codyk12 <cody.kesler@icloud.com>
2019-08-19 15:33:53 -0400

    Fixed color and image type requirement to be the same

--- src/imagemap.jl
+++ src/imagemap.jl
@@ -5,7 +5,7 @@ Places an smaller image on the given image, only places pixels with in the image
 
 ImageDraw.draw!(img::AbstractArray{T,2}, map::ImageMap) where {T<: Colorant} = draw!(img::AbstractArray{T,2}, map::ImageMap, oneunit(T))
 
-function ImageDraw.draw!(img::AbstractArray{T,2}, map::ImageMap, color::T, mask::Bool=false) where {T<:Colorant}
+function ImageDraw.draw!(img::AbstractArray{T,2}, map::ImageMap, color::R, mask::Bool=false) where {R,T<:Colorant}
     size(img) < size(map.image) && error("ImageMap must have a smaller image than the canvas image.")
     s = size(map.image)
     n = size(img)[1]
