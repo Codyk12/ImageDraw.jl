cb9ca3d863bbe3d8682a060a7b996eb653e319c5
Don March <don@ohspite.net>
2019-09-03 03:00:50 -0400

    Simplify indexing with CartesianIndices
    This also should support custom arrays with non-standard indexing.

--- src/imagemap.jl
+++ src/imagemap.jl
@@ -1,25 +1,14 @@
 function ImageDraw.draw!(img::AbstractArray{T,2}, map::ImageMap, color::T=oneunit(T); mask::Bool=false) where {R,T<:Colorant}
     size(img) < size(map.image) && error("ImageMap must have a smaller image than the canvas image.")
-    s = size(map.image)
-    n, m = size(img)
-    x = map.x - ceil(Int, s[1]/2)
-    y = map.y - ceil(Int, s[2]/2)
 
-    x_start = max(x, 1)
-    y_start = max(y, 1)
+    map_center = (map.x, map.y)
+    half_map_size, maybe_extra_pixel = zip(divrem.(size(map.image), 2)...)
+    map_offset = CartesianIndex(map_center .- half_map_size)
 
-    x_end = min(x+s[1]-1, n)
-    y_end = min(y+s[2]-1, m)
+    p1 = max.(first.(axes(img)), first.(axes(img)) .+ map_offset.I)
+    p2 = min.(last.(axes(img)), first.(axes(img)) .+ map_offset.I .+ size(map.image) .- (1, 1))
 
-    mask ? (img[x_start:x_end,y_start:y_end] .= color; return img) : nothing
-
-    m_x_start = x_start == 1 ? x_start - x + 1 : 1
-    m_y_start = y_start == 1 ? y_start - y + 1 : 1
-
-    m_x_end = x_end == n ? x+s[1]-1 - n : 0
-    m_y_end = y_end == m ? y+s[2]-1 - n : 0
-
-    img[x_start:x_end,y_start:y_end] = map.image[m_x_start:end-m_x_end,m_y_start:end-m_y_end]
-
-    img
+    rng = CartesianIndices((p1[1]:p2[1], p1[2]:p2[2]))
+    copyto!(img, rng, map.image, rng .- map_offset)
+    return img
 end
